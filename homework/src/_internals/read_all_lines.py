c086ad193309c2d3b0a6770cb6f51a92d40c6527
Jessica Paola Vega <111544730+Jevega14@users.noreply.github.com>
2026-05-16 10:00:41 -0500

    Refactor read_all_lines to improve variable usage

--- homework/src/_internals/read_all_lines.py
+++ homework/src/_internals/read_all_lines.py
@@ -2,10 +2,11 @@ import os
 
 
 def read_all_lines(input_folder):
-    """Read all lines from all files in the input folder."""
-    lines = []
-    for filename in os.listdir(input_folder):
+    all_lines = []
+    input_file_list = os.listdir(input_folder)
+    for filename in input_file_list:
         file_path = os.path.join(input_folder, filename)
-        with open(file_path, "r", encoding="utf-8") as f:
-            lines.extend(f.readlines())
-    return lines
+        with open(file_path, "r", encoding="utf8") as f:
+            lines = f.readlines()
+            all_lines.extend(lines)
+    return all_lines
