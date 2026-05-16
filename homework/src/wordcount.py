5aa0b07099eb3bed9602f049eafde9d7c6774487
Jessica Paola Vega <111544730+Jevega14@users.noreply.github.com>
2026-05-16 09:53:25 -0500

    Fix import statement for preprocess_lines

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,7 +1,7 @@
 import sys
 
 from ._internals.count_words import count_words
-from ._internals.preprocess_lines import preprocess_lines
+from ._internals.preprocess_lines import preprocess_lines /
 from ._internals.read_all_lines import read_all_lines
 from ._internals.split_into_words import split_into_words
 from ._internals.write_word_counts import write_word_counts
